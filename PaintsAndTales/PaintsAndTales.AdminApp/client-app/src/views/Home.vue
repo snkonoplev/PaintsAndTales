de88d70e2df8858b3af534c84293e5d0a1eb8300
Sergey Konoplev <sn.konoplev@gmail.com>
2019-11-08 17:39:33 +0300

    Charts

--- PaintsAndTales/PaintsAndTales.AdminApp/client-app/src/views/Home.vue
+++ PaintsAndTales/PaintsAndTales.AdminApp/client-app/src/views/Home.vue
@@ -1,4 +1,16 @@
 <template>
-  <div class="home">
-  </div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-3">
+				<sales-chart></sales-chart>
+			</div>
+		</div>		
+	</div>	
 </template>
+<script>
+import SalesChart from '../components/SalesChart'
+
+export default {
+	components: {SalesChart}
+}
+</script>
